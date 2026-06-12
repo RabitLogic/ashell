1847736d6602dd56191b122a6c1f10b53aa805f2
ancion <ancion@github.com>
2026-06-12 19:59:50 +0800

    merge fix

--- src/config.rs
+++ src/config.rs
@@ -123,7 +123,8 @@ pub struct ConfigFile {
     pub workspace_panels: Option<Vec<f32>>,
     #[serde(default)]
     pub body_panels: Option<Vec<f32>>,
-
+    #[serde(default)]
+    pub transfers: Vec<crate::terminal::Transfer>,
 }
 
 fn default_locale() -> String {
@@ -260,6 +261,16 @@ impl ConfigStore {
         self.cache.body_panels.as_ref()
     }
 
+    pub fn transfers(&self) -> Vec<crate::terminal::Transfer> {
+        self.cache.transfers.clone()
+    }
+
+    pub fn set_transfers(&mut self, transfers: Vec<crate::terminal::Transfer>) {
+        self.cache.transfers = transfers;
+        if let Err(err) = self.save() {
+            tracing::error!("failed to save config: {err:#}");
+        }
+    }
 
     pub fn set_layout_state(
         &mut self,
